7be7e80c9f4919f2e324c0ef919ef6caf8de8819
Ricky Sheaves <r@rocketsquawk.com>
2013-06-16 15:09:26 -0400

    Add code, license, and flesh out README

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-gitweb2gitlab
-=============
+# Gitweb2GitLab
 
-Migration script that scrapes a gitweb instance and moves all its repositories to a GitLab instance.
+A migration script written in Ruby that scrapes a gitweb instance and moves all its repositories to a GitLab instance.
+
+This code is quick and dirty. It's not pretty. It's not elegant. In a nutshell, it:
+
+* Uses Nokogiri to find all the repo names on the gitweb home page
+* Uses Nokogiri again to access each repo summary page in gitweb to get the description
+* Clones each repo
+* Uses RestClient to create a GitLab project
+* Sets up the GitLab server as a remote in the new clone
+* Pushes the dev and master branches and all tags to GitLab
+* Sets some branch permissions and manipulates the project's users
+* Rinse and repeat
+
+As mentioned, no style points will be awarded, but this got the job done for me with about 200 projects.
+
+## Dependencies
+
+* Gitlab >5.0
+* Ruby 1.9.3
+* Nokogiri, RestClient, and JSON gems
+* A gitweb instance to migrate from (duh)
+
+## Notes
+
+Change the handy slugs to suit your environment:
+
+	# Handy global slugs
+	gitlab_url = 'http://rocketsquawk.dyndns.org/api/v3/'
+	api_key = 'oWMTPkERQxKcTenV2aNT'
+	gitweb_url = 'http://git/git'
+
+#### gitlab_url
+
+The URL to your GitLab instance including the API version number
+
+#### api_key
+
+Use the GitLab admin user's API key
+
+#### gitweb_url
+
+The URL to your gitweb instance
+
+## Oh, and ...
+
+No, none of the URLs or API keys in this script are valid anymore :P
---
+++ migrate.rb
@@ -0,0 +1,64 @@
+require 'nokogiri'
+require 'open-uri'
+require 'rest_client'
+require 'json'
+
+# Handy global slugs
+gitlab_url = 'http://rocketsquawk.dyndns.org/api/v3/'
+api_key = 'oWMTPkERQxKcTenV2aNT'
+gitweb_url = 'http://git/git'
+
+begin
+	gitweb_home = Nokogiri::HTML(open(gitweb_url))
+rescue
+	puts "Something very bad happened; I couldn't even connect to the Git server."
+end
+
+gitweb_home.css('a.list').each do |project|
+	# We only care about the nodes that have git repo names
+	if project.content.end_with?('.git')
+		
+		begin
+			# Some handy project-level slugs
+			p = project.content.chomp('.git')
+			cd = 'cd ' + p + ' && '
+			
+			# Get the repo description
+			proj_page = Nokogiri::HTML(open(gitweb_url + '/?p=' + project.content + ';a=summary'))
+			desc = proj_page.css('tr#metadata_desc').inner_text.gsub(/\Adescription/, '')
+			
+			# Clone the repo and switch to dev branch
+			system('git clone git@git:' + p)
+			system(cd + 'git checkout dev')
+
+			# Create project in GitLab
+			r = RestClient.post(gitlab_url + 'projects', \
+				{:name => p, :description => desc, :namespace_id => 'GLN'}, \
+				{:'PRIVATE-TOKEN' => api_key})
+
+			# Get the project ID from the JSON response
+			id =JSON.parse(r)["id"]
+
+			# Add user_id 2 (ricky) to the project team
+			# One could make an API call to discover the user_id, but I already know it
+			RestClient.post(gitlab_url + 'projects/' + id.to_s + '/members', \
+				{:id => id, :user_id => '2', :access_level => '40'}, \
+				{:'PRIVATE-TOKEN' => api_key})
+
+			# Add GitLab as a remote and push 
+			system(cd + 'git remote add gitlab git@rocketsquawk.dyndns.org:' + \
+				p.downcase.gsub('.', '-') + '.git') # If your repos have periods in their names, you need this
+			system(cd + 'git push -u gitlab --all')
+			system(cd + 'git push -u gitlab --tags')
+			
+			# Lock down the master branch
+			RestClient.put(gitlab_url + 'projects/' + id.to_s + \
+					'/repository/branches/master/protect', \
+				{:id => id, :branch => 'master'}, \
+				{:'PRIVATE-TOKEN' => api_key})
+		rescue => e 
+			puts "### WOOPS! ### Something went wrong: " + e.message
+		end
+	end
+end
+
